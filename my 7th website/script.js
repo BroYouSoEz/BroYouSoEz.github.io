1a3c5ad9127586bab68bd949ca724029f8da9788
BroYouSoEz <54675580+BroYouSoEz@users.noreply.github.com>
2019-12-16 17:03:00 +0800

    ㄅ

---
+++ my 7th website/script.js
@@ -0,0 +1,31 @@
+function check(event){
+    event.preventDefault();
+
+    var errorLog ="";
+    var name = $("#userName").val();
+    var email = $("#userEmail").val();
+    var passw = $("#userPassword").val();
+    var cpassw = $("#checkPassword").val();
+    var message = $("#textArea").val();
+
+    if (name == "" || email == "" || passw =="" || cpassw == "" || message == "") {
+        errorLog = "表格不可留空!";
+    }else if(!checkEmail(email)){
+        errorLog = "請輸入正確的E-mail!";
+    }else if(passw != cpassw){
+        errorLog = "請確認密碼是否正確!";
+    }
+
+    if(errorLog !=""){
+        $("#error").css("color","red");
+        $("#error").html(errorLog);
+    }else{
+        $("#error").css("color","lightgreen");
+        $("#error").html("表單已送出!");
+    }
+}
+
+function checkEmail(Email){
+    var checker = /^\b[A-Z0-9]+@[A-Z0-9]+\.com\b/i; //正規表達式 會去偵測值是否有達成
+    return checker.test(Email);
+}
